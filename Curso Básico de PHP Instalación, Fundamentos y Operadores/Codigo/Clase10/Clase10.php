285ea0c07980260898f79897f81f3a65b8c81604
Antonio Natanael <cs21062@ues.edu.sv>
2026-01-17 22:40:19 -0600

    Se agregan los archivos de las clases 8, 9 y 10, y sus respectivos archivos de codigo trabajados.

---
+++ Curso Básico de PHP Instalación, Fundamentos y Operadores/Codigo/Clase10/Clase10.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Hola desde Clase10.php \n\n";
+
+$personas = [
+    "Natanael" => 29,
+    "Ana" => 25,
+    "Luis" => 30
+];
+
+echo "Inspeccionando el array de personas con var_dump:\n";
+var_dump($personas);
+
+echo "\nInspeccionando el array de personas con print_r:\n";
+print_r($personas);
+
+//Comentario de una sola línea
+# Comentario de una sola línea
+/* Comentario
+   de múltiples lineas */
---
+++ Curso Básico de PHP Instalación, Fundamentos y Operadores/Codigo/Clase9/Clase9.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Hola, esta es la Clase 9 de PHP. \n";
+
+$nombre = "Natanael";
+$apellido = "Sánchez";
+
+//Usa <br> para salto de línea en HTML
+//Usa \n para salto de línea en consola
+
+//Concatenación
+//Concatena con punto (.)
+echo "No, no. yo me llamo " . $nombre . " " . $apellido . ".\n";
+
+//Interpolación
+echo "No, no. yo me llamo $nombre $apellido.\n"; 
+
+//Operaciones matematicas
+echo "El resultado de 4 x 5 es: " . (4 * 5) . "\n";
+
+//Etiqueta de cierre no es necesaria si solo se utilizara codigo php?>
